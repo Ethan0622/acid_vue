8cd8bc39ae91c8793bace550ceb7099f683a529e
heoizokdung <xzd333333@163.com>
2020-12-27 19:57:06 +0800

    chore: 修复操作 git 导致的代码错误

--- src/plugins/dialog/dialog.vue
+++ src/plugins/dialog/dialog.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="dialog">
-    <narrative-bubble-vue class="bubble" @click="next" :clickable="!this.typer.typing">
+    <dialog-bubble-vue class="bubble" @click="next" :clickable="!this.typer.typing">
       <p class="text" v-html="typed"></p>
-    </narrative-bubble-vue>
+    </dialog-bubble-vue>
     <div class="choices" v-if="showChoicesView">
       <div v-for="choice in paragraph.responses" :key="choice.name" @click="feedback(choice)" class="choice">
         {{ choice.name }}
